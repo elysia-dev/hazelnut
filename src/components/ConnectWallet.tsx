c0746aab6f3569d3919df67686dc71780f295d3c
hyemmie <lhm0710@naver.com>
2021-01-05 21:04:15 +0900

    modify: logo position to center

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -14,40 +14,45 @@ function ConnectWallet(props: Props) {
         style={{
           height: '100%',
           padding: '5% 5%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <div
-          style={{
-            borderRadius: '50%',
-            width: 140,
-            height: 140,
-            boxShadow: '0 1px 6px #00000029',
-            margin: 'auto',
-            marginTop: 70,
-          }}
-        >
-          <div style={{ margin: 'auto', width: 90, padding: 25 }}>
-            <img
-              src={Logo}
-              style={{
-                width: 90,
-                position: 'relative',
-                top: -3,
-                left: 1,
-              }}
-            />
+        <div style={{ flex: 1, justifyItems: 'center', alignItems: 'center' }}>
+          <div
+            style={{
+              borderRadius: '50%',
+              width: 140,
+              height: 140,
+              boxShadow: '0 1px 6px #00000029',
+              margin: 'auto',
+              marginTop: '50%',
+            }}
+          >
+            <div style={{ margin: 'auto', width: 90, padding: 25 }}>
+              <img
+                src={Logo}
+                style={{
+                  width: 90,
+                  position: 'relative',
+                  top: -3,
+                  left: 1,
+                }}
+              />
+            </div>
           </div>
-        </div>
-        <div
-          style={{
-            fontSize: 25,
-            textAlign: 'center',
-            marginTop: 30,
-            fontWeight: 900,
-            color: '#1C1C1C',
-          }}
-        >
-          ELYSIA
+          <div
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginTop: 30,
+              fontWeight: 900,
+              color: '#1C1C1C',
+            }}
+          >
+            ELYSIA
+          </div>
         </div>
       </div>
       <div
