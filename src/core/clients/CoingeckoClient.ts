74640f18303efaa3b01fac6fd3b2acfae4ae34f8
donguk.seo <donguk.seo@modori.io>
2021-01-22 14:08:32 +0900

    Add getEthPrice

--- src/core/clients/CoingeckoClient.ts
+++ src/core/clients/CoingeckoClient.ts
@@ -6,8 +6,21 @@ type Response = {
   };
 };
 
+type EthResponse = {
+  ethereum: {
+    usd: number;
+  };
+};
+
 export const getElPrice = (): Promise<AxiosResponse<Response>> => {
   return Axios.get(
     'https://api.coingecko.com/api/v3/simple/price?ids=elysia&vs_currencies=usd',
   );
 };
+
+export const getEthPrice = (): Promise<AxiosResponse<EthResponse>> => {
+  return Axios.get(
+    'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd',
+  );
+};
+
