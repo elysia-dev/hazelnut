afb22017a803bf8d2931856a48187542ca373a2f
donguk.seo <donguk.seo@modori.io>
2021-01-27 13:18:12 +0900

    Refactor price oracle

--- src/hooks/usePrice.ts
+++ src/hooks/usePrice.ts
@@ -4,31 +4,34 @@ import PRICE_ORACLE_ABI from '../core/constants/abis/price-oracle.json';
 import useContract from "./useContract";
 
 export function useEthPrice(): number {
-  const [price, setPrice] = useState<number>(1212);
-  const priceOracle = useContract(process.env.REACT_APP_EL_PRICE_ORACLE_ADDRESS, PRICE_ORACLE_ABI, false)
-
-  useEffect(() => {
-    priceOracle?.getPrice().then((res: BigNumber) => {
-      const price = res.div(`1${'0'.repeat(18)}`).toNumber()
-
-      if (price > 0) {
-        setPrice(price)
-      }
-    }).catch((e: any) => {
-      console.log(e);
-    })
-  }, [priceOracle])
-
-  return price;
+  return usePrice(
+    1212,
+    process.env.REACT_APP_ETH_PRICE_ORACLE_ADDRESS || '',
+    PRICE_ORACLE_ABI
+  )
 }
 
 export function useElPrice(): number {
-  const [price, setPrice] = useState<number>(0.03);
-  const priceOracle = useContract(process.env.REACT_APP_EL_PRICE_ORACLE_ADDRESS, PRICE_ORACLE_ABI, false)
+  return usePrice(
+    0.03,
+    process.env.REACT_APP_EL_PRICE_ORACLE_ADDRESS || '',
+    PRICE_ORACLE_ABI
+  )
+}
+
+export default function usePrice(
+  initialValue: number,
+  address: string,
+  abi: object,
+): number {
+  const [price, setPrice] = useState<number>(initialValue);
+  const priceOracle = useContract(address, abi, false)
 
   useEffect(() => {
     priceOracle?.getPrice().then((res: BigNumber) => {
       const price = res.div(`1${'0'.repeat(18)}`).toNumber()
+
+      console.log(price)
 
       if (price > 0) {
         setPrice(price)
@@ -36,7 +39,7 @@ export function useElPrice(): number {
     }).catch((e: any) => {
       console.log(e)
     })
-  }, [])
+  }, [priceOracle])
 
   return price;
 }
