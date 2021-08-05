cd1866401a978ce42a58c361643aebd43d93db00
leegakyeong <alaska7132@naver.com>
2021-08-05 17:00:03 +0900

    Add staking page

---
+++ src/containers/StakingRequests.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type ParamTypes = {
+  imValue: string;
+  imType: string
+  imContractAddress: string;
+  imEthAddresses: string;
+  imLanguage: string;
+};
+
+type TransferInfoTypes = {
+  value: number,
+  type: string,
+  contractAddress: string,
+  ethAddress: string,
+  language: string,
+}
+
+const StakingRequests: React.FC = () => {
+  return (
+    <div>hello</div>
+  );
+}
+
+export default StakingRequests;
