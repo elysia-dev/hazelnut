37522279907bade6c54c981351a4338e2fdf1435
leegakyeong <alaska7132@naver.com>
2021-08-17 11:09:27 +0900

    Update Unstake page

--- src/containers/Unstake.tsx
+++ src/containers/Unstake.tsx
@@ -1,21 +1,153 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StakingTransactionRequest from '../core/types/StakingTransactionRequest';
+import { useTranslation } from 'react-i18next';
+import { BigNumber, BigNumberish, constants, utils } from 'ethers';
+import { PopulatedTransaction } from '@ethersproject/contracts';
+import { useWeb3React } from '@web3-react/core';
+import InjectedConnector from '../core/connectors/InjectedConnector';
+import useContract, { useElysiaToken, useElfiToken } from '../hooks/useContract';
+import ConnectWallet from '../components/ConnectWallet';
+import TxSummary from '../components/TxSummary';
+import Button from '../components/Button';
+import BoxLayout from '../components/BoxLayout';
+import AddressBottomTab from '../components/AddressBottomTab';
+import Swal, { RetrySwal } from '../core/utils/Swal';
+import RefundSuccess from './../images/success_refund.svg';
+import { changeEthNet, isValidChainId } from '../core/utils/createNetwork';
+import PaymentMethod from '../core/types/PaymentMethod';
+import STAKING_POOL_ABI from '../core/constants/abis/staking-pool.json';
 
 const Unstake: React.FC<{ transactionRequest: StakingTransactionRequest }> = ({ transactionRequest }) => {
-  return (
-    <div>
-      <h1>Unstake</h1>
-      <div>{transactionRequest.value}</div>
-      <div>{transactionRequest.type}</div>
-      <div>{transactionRequest.unit}</div>
-      <div>{transactionRequest.round}</div>
-      <div>{transactionRequest.contractAddress}</div>
-      <div>{transactionRequest.userAddress}</div>
-      <div>{transactionRequest.language}</div>
-      <div>{transactionRequest.rewardValue}</div>
-      <div>{transactionRequest.migrationValue}</div>
-    </div>
+  const { t } = useTranslation();
+  const { activate, library, account } = useWeb3React();
+  const elToken = useElysiaToken();
+  const elfiToken = useElfiToken();
+  const tokenContract = transactionRequest.unit === 'EL' ? elToken : elfiToken;
+  const stakingPoolContract = useContract(
+    transactionRequest.contractAddress || '',
+    STAKING_POOL_ABI,
   );
+  const [chainId, setChainId] = useState<string>('');
+
+  const currentChainId = async () => {
+    setChainId(await library.provider.request({
+      method: 'eth_chainId'
+    }));
+  };
+
+  const networkCheck = () => {
+    return isValidChainId(
+      transactionRequest.unit || '',
+      chainId,
+    );
+  };
+
+  const createNetwork = async () => {
+    if (chainId === process.env.REACT_APP_ETH_NETWORK) return;
+    await changeEthNet(library);
+  };
+
+  const checkAccount = () => {
+    RetrySwal.fire({
+      html: `<div style="font-size:15px; margin-top: 20px;">
+        ${t('Error.CheckAccount')}
+        </div>`,
+      icon: 'info',
+      confirmButtonText: t('Error.Check'),
+      showCloseButton: true,
+    });
+  };
+
+  const createTransaction = () => {
+    if(!networkCheck()){
+      createNetwork();
+      return;
+    }
+
+    stakingPoolContract?.populateTransaction
+      .withdraw(
+        BigNumber.from(transactionRequest.value),
+        transactionRequest.round
+      )
+      .then(populatedTransaction => {
+        sendTransaction(populatedTransaction);
+      });
+  };
+
+  const sendTransaction = (populatedTransaction: PopulatedTransaction) => {
+    library.provider
+      .request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            to: populatedTransaction.to,
+            from: account,
+            data: populatedTransaction.data,
+          },
+        ],
+      })
+      .then(() => {
+        Swal.fire({
+          title: t('Completion.Title'),
+          html: `<div style="font-size: 15px;"> ${t('Completion.UnstakingResult', {
+            stakingTokenType: transactionRequest.unit,
+            amount: transactionRequest.value,
+          })}<br />${t('Completion.Notice')}</div>
+          `,
+          imageUrl: RefundSuccess,
+          imageWidth: 275,
+          showConfirmButton: false,
+          // allowOutsideClick: false,
+        });
+      })
+      .catch(() => {
+        Swal.fire({
+          text: t('Error.TransactionCancled'),
+          icon: 'error',
+          confirmButtonText: t('Buying.TransactionRetryButton'),
+          showCloseButton: true,
+        }).then(res => {
+          if (res.isConfirmed) {
+            createTransaction();
+          }
+        });
+      });
+  };
+
+  useEffect(() => {
+    if (!account && !chainId) return;
+    currentChainId();
+    if(!chainId) return;
+    createNetwork();
+    // Swal.close();
+  }, [account, chainId]);
+
+  if (!account) {
+    return <ConnectWallet handler={() => activate(InjectedConnector)} />;
+  } else {
+    return (
+      <div>
+        <BoxLayout>
+          <div style={{ padding: 20 }}>
+            <Button
+              style={{ marginTop: 20 }}
+              clickHandler={() => {
+                account &&
+                String(transactionRequest.userAddress) !== account
+                  ? checkAccount()
+                  : createTransaction();
+              }}
+              title={t('Buying.TransactionRetryButton')}
+            />
+          </div>
+        </BoxLayout>
+        <AddressBottomTab
+          chainId={chainId}
+          paymentMethod={transactionRequest.unit === 'EL' ? PaymentMethod.EL : PaymentMethod.ELFI}
+        />
+      </div>
+    );
+  }
 }
 
 export default Unstake;
