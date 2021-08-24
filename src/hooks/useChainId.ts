6950604990eedb855da601ffdf23f586598edf76
leegakyeong <alaska7132@naver.com>
2021-08-24 16:54:04 +0900

    Add useEffect() to useChainId()

--- src/hooks/useChainId.ts
+++ src/hooks/useChainId.ts
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
 
 function useChainId() {
   const [chainId, setChainId] = useState<string>('');
   const { library } = useWeb3React();
 
-  if (library) { // 지갑에 연결되면
-    library.provider.request({ method: 'eth_chainId' })
+  useEffect(() => {
+    library?.provider.request({ method: 'eth_chainId' })
     .then((res: any) => setChainId(res));
-  }
+  }, [library]);
 
   return chainId;
 }
