c34fcf4fd3832d39649da84a2c7c92a48b7a43b3
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-10-19 14:22:19 +0900

    Modify url to env

--- src/core/utils/saveTxData.ts
+++ src/core/utils/saveTxData.ts
@@ -10,18 +10,15 @@ export const saveTxData = (
   unit?: string,
 ) => {
   axios
-    .post(
-      `https://pyyrhyhqub.execute-api.us-east-1.amazonaws.com/channel/${uuid}/tx`,
-      {
-        transferType,
-        date: String(new Date()),
-        unit: unit?.toUpperCase() || '',
-        nonce: 0,
-        txHash,
-        amount,
-        cryptoType: cryptoType.toUpperCase(),
-      },
-    )
+    .post(`${process.env.REACT_APP_UUID_DATA_URL}/${uuid}/tx`, {
+      transferType,
+      date: String(new Date()),
+      unit: unit?.toUpperCase() || '',
+      nonce: 0,
+      txHash,
+      amount,
+      cryptoType: cryptoType.toUpperCase(),
+    })
     .then(res => {})
     .catch(error => {});
 };
