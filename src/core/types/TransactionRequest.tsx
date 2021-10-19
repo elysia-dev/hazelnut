094f0efe87bad0fd2c30d92c98f04f4a3a235920
정규철 <stussy@jeong-gyucheols-MacBook-Pro.local>
2021-10-19 13:26:06 +0900

    Add uuid type

--- src/core/types/TransactionRequest.tsx
+++ src/core/types/TransactionRequest.tsx
@@ -1,28 +1,29 @@
-import TransactionType from "../enums/TransactionType";
-import LanguageType from "../enums/LanguageType";
-import PaymentMethod from "./PaymentMethod";
+import TransactionType from '../enums/TransactionType';
+import LanguageType from '../enums/LanguageType';
+import PaymentMethod from './PaymentMethod';
 
 type TransactionRequest = {
   type: TransactionType | string;
-  amount: number
+  amount: number;
   userAddresses: string | string[] | null;
   language?: LanguageType | string;
   product: {
-    title: string
-    tokenName: string
-    expectedAnnualReturn: string
-    contractAddress: string
-    usdPricePerToken: number
-    paymentMethod: PaymentMethod
+    title: string;
+    tokenName: string;
+    expectedAnnualReturn: string;
+    contractAddress: string;
+    usdPricePerToken: number;
+    paymentMethod: PaymentMethod;
     data: {
-      images: string[]
-    }
-  }
+      images: string[];
+    };
+  };
   contract: {
     address: string | string[] | null;
-    abi: string
-    version: string
-  }
-}
+    abi: string;
+    version: string;
+  };
+  uuid: string;
+};
 
 export default TransactionRequest;
